f54c3deaec137b94e6308d4d2ef66eaf672e8d28
Andres <andres_ge@hotmail.es>
2018-05-19 10:11:13 -0500

    PassThrough Filter with ideal values

--- Exercise-1/RANSAC.py
+++ Exercise-1/RANSAC.py
@@ -5,13 +5,14 @@ import pcl
 cloud = pcl.load_XYZRGB('tabletop.pcd')
 
 # -------------------------------------------- First Modification --------------------------------------------------
+# Voxel Grid filter
 # Create a VoxelGrid filter object for our input point cloud
 vox = cloud.make_voxel_grid_filter()
 
 # Choose a voxel (also known as leaf) size
 # Note: this (1) is a poor choice of leaf size
 # Experiment and find the appropriate size!
-LEAF_SIZE = 0.1  # Reduces the number of points (filter of the orignal image).
+LEAF_SIZE = 0.01  # Reduces the number of points (filter of the orignal image).
 
 # Set the voxel (or leaf) size
 vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
@@ -22,11 +23,23 @@ filename = 'voxel_downsampled.pcd'
 pcl.save(cloud_filtered, filename)
 # -------------------------------------------- First Modification End --------------------------------------------------
 
-# Voxel Grid filter
-
-
+# -------------------------------------------- Second Modification --------------------------------------------------
 # PassThrough filter
+# Create a PassThrough filter object.
+passthrough = cloud_filtered.make_passthrough_filter()
 
+# Assign axis and range to the passthrough filter object.
+filter_axis = 'z'
+passthrough.set_filter_field_name(filter_axis)
+axis_min = 0.6
+axis_max = 1.1
+passthrough.set_filter_limits(axis_min, axis_max)
+
+# Finally use the filter function to obtain the resultant point cloud.
+cloud_filtered = passthrough.filter()
+filename = 'pass_through_filtered.pcd'
+pcl.save(cloud_filtered, filename)
+# --------------------------------------------Second Modification End --------------------------------------------------
 
 # RANSAC plane segmentation
 
